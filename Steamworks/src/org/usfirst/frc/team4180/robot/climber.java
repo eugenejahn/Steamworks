e3b0d357b01ea951cd21ced18459826d60abe002
eugenejahn <eugenejahnjahn@gmail.com>
2017-01-19 18:26:55 -0800

    Create climber.java

---
+++ Steamworks/src/org/usfirst/frc/team4180/robot/climber.java
@@ -0,0 +1,25 @@
+public class climber {
+
+		private VictorSP  ClimbVictor;
+		
+		
+		public climb(int VictorPort) {
+			ClimbVictor = new VictorSP(VictorPort);
+			
+		}
+		
+		public void updateSpeed(double[] JoystickInfo) {
+			double x = JoystickInfo[0];
+			double y = JoystickInfo[1];
+			
+			ClimbVictor.set( (y - x) * .50); 
+				 
+			
+		
+			
+			}
+		}
+		
+
+
+
